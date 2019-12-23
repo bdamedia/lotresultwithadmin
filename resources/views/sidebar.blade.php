a01bbee53bc15006b86fdfee699fefef47b0119d
Sumit Sharma <sumit.sharma@bda.media>
2019-12-23 13:05:46 +0530

    Update inti

---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,154 @@
+<div class="hidden-xs hidden-sm col-md-3 center-side-bar fix-width-center">
+
+    <h2 class="hide">Center side bar</h2>
+    <div class="item-menu">
+        <div class="title-item">
+            <h3><a href="/cau-mien-bac/cau-bach-thu.html" title="Thống Kê Cầu">Thống Kê Cầu</a></h3>
+        </div>
+        <div class="content-item"><ul>
+
+                <li><a href="/cau-mien-bac/cau-bach-thu.html" title="Cầu bạch thủ MB">Cầu bạch thủ ( MB )</a></li>
+                <li><a href="/cau-mien-bac/cau-lat-lien-tuc.html" title="Cầu lật liên tục MB">Cầu lật liên tục ( MB )</a></li>
+                <li><a href="/cau-mien-bac/cau-ve-ca-cap.html" title="Cầu về cả cặp MB">Cầu về cả cặp ( MB )</a></li>
+                <li><a href="/cau-mien-bac/cau-ve-nhieu-nhay.html" title="Cầu về nhiều nháy MB">Cầu về nhiều nháy ( MB )</a></li>
+                <li><a href="/cau-mien-nam.html" title="Cầu miền Nam">Cầu miền Nam</a></li>
+                <li><a href="/cau-mien-trung.html" title="Cầu miền Trung">Cầu miền Trung</a></li>
+            </ul></div></div>
+    <div class="item-menu">
+        <div class="title-item"><h3><a href="/thong-ke-lo-gan.html" title="Thống Kê Lô">Thống Kê Lô</a></h3></div>
+        <div class="content-item"><ul>
+                <li><a href="/thong-ke-lo-gan.html" title="TK lô gan">TK lô gan</a></li>
+                <li><a href="/thong-ke-lo-xien.html" title="TK lô xiên">TK lô xiên</a></li>
+                <li><a href="/thong-ke-lo-kep.html">TK lô kép</a></li>
+                <li><a href="/thong-ke-dau.html" title="TK lô đầu">TK lô đầu</a></li>
+                <li><a href="/thong-ke-duoi.html" title="TK lô đuôi">TK lô đuôi</a></li>
+                <li><a href="/thong-ke-giai-dac-biet.html">TK giải đặc biệt</a></li>
+                <li><a href="/thong-ke-tan-suat.html" title="TK lần xuất hiện">TK lần xuất hiện</a></li>
+                <li><a href="/thong-ke-00-99.html" title="TK 00 - 99">TK 00 - 99</a></li>
+                <li><a href="/thong-ke-theo-chu-ky-4-1-0.html" title="TK chu kỳ" target="_blank">TK chu kỳ</a></li>
+            </ul></div>
+    </div>
+    <div class="item-menu">
+        <div class="title-item"><h3><a href="/thong-ke-lan-xuat-hien.html">Thống Kê Vietlott</a></h3></div>
+        <div class="content-item"><ul>
+                <li><a href="/thong-ke-lan-xuat-hien.html" class="text-black-bold">TK xổ số Mega 6/45</a>
+                    <ul class="sub-content">
+                        <li><a href="/thong-ke-lan-xuat-hien.html" title="TK lần xuất hiện">TK lần xuất hiện</a></li>
+                        <li><a href="/thong-ke-theo-thu.html" title="TK theo thứ">TK theo thứ</a></li>
+                    </ul></li>
+                <li><a href="/thong-ke-theo-luot-ve.html" class="text-black-bold">TK xổ số Max 4D</a>
+                    <ul class="sub-content">
+                        <li><a href="/thong-ke-theo-luot-ve.html" title="TK theo lượt về">TK theo lượt về</a></li>
+                        <li><a href="/thong-ke-theo-long-cau.html" title="TK theo lồng cầu">TK theo lồng cầu</a></li>
+                    </ul></li>
+            </ul></div></div>
+    <div class="item-menu">
+        <div class="title-item"><h3><a href="/xsmb-truc-tiep.html">Xổ Số Trực Tiếp Hôm Nay</a></h3></div>
+        <div class="content-item"><ul>
+                <li><a href="/xsmn-xo-so-mien-nam.html" title="XSMN 16h10" class="text-black-bold">XSMN ( 16h10 ) </a>
+                    <ul class="sub-content"><li><a href="/xsvt-xo-so-vung-tau.html" title="Vũng Tàu">Vũng Tàu</a></li>
+                        <li><a href="/xsbtr-xo-so-ben-tre.html" title="Bến Tre">Bến Tre</a></li>
+                        <li><a href="/xsbl-xo-so-bac-lieu.html" title="Bạc Liêu">Bạc Liêu</a></li>
+                    </ul></li>
+                <li><a href="/xsmt-xo-so-mien-trung.html" title="XSMT 17h15" class="text-black-bold">XSMT ( 17h15 ) </a>
+                    <ul class="sub-content"><li><a href="/xsdlk-xo-so-dak-lak.html" title="Đắk Lắk">Đắk Lắk</a></li>
+                        <li><a href="/xsqna-xo-so-quang-nam.html" title="Quảng Nam">Quảng Nam</a></li>
+                    </ul></li>
+                <li><a href="/xsmb-xo-so-mien-bac.html" title="XSMB 18h15" class="text-black-bold">XSMB ( 18h15 ) </a>
+                    <ul class="sub-content"><li><a href="/xsmb-xo-so-mien-bac.html" title="Miền Bắc">Miền Bắc</a></li>
+                    </ul>
+                </li></ul></div></div>
+    <div class="item-menu">
+        <div class="title-item"><h3><a href="/xsmb-xo-so-mien-bac.html" title="Xổ Số Hôm Qua">Xổ Số Hôm Qua</a></h3></div>
+        <div class="level2"><ul><li><a href="/xsmb-xo-so-mien-bac.html" title="Miền Bắc">Miền Bắc</a></li>
+                <li><a href="/xstth-xo-so-hue.html" title="Huế">Huế</a></li>
+                <li><a href="/xspy-xo-so-phu-yen.html" title="Phú Yên">Phú Yên</a></li>
+                <li><a href="/xsdt-xo-so-dong-thap.html" title="Đồng Tháp">Đồng Tháp</a></li>
+                <li><a href="/xshcm-xo-so-tphcm.html" title="TP.HCM">TP.HCM</a></li>
+                <li><a href="/xscm-xo-so-ca-mau.html" title="Cà Mau">Cà Mau</a></li>
+            </ul></div></div>
+    <div class="item-menu">
+        <div class="title-item"><h3><a href="/xo-so-theo-dai.html" title="Xổ Số Hôm Qua">KQXS theo tỉnh</a></h3></div>
+        <div class="level2"><ul>
+
+                <li><a href="javascript:void(0)">Miền Bắc</a><i class="fa fa-caret-down" aria-hidden="true"></i>
+                    <ul class="level3"><li><a href="/xsbn-xo-so-bac-ninh.html">Bắc Ninh</a></li><li><a href="/xstd-xo-so-ha-noi.html">Hà Nội</a></li><li><a href="/xshp-xo-so-hai-phong.html">Hải Phòng</a></li><li><a href="/xsnd-xo-so-nam-dinh.html">Nam Định</a></li><li><a href="/xsqn-xo-so-quang-ninh.html">Quảng Ninh</a></li><li><a href="/xstb-xo-so-thai-binh.html">Thái Bình</a></li></ul>
+                </li>
+                <li><a href="javascript:void(0)">Miền Trung</a><i class="fa fa-caret-down" aria-hidden="true"></i>
+                    <ul class="level3"><li><a href="/xsbdi-xo-so-binh-dinh.html">Bình Định</a></li><li><a href="/xsdna-xo-so-da-nang.html">Đà Nẵng</a></li><li><a href="/xsdlk-xo-so-dak-lak.html">Đắk Lắk</a></li><li><a href="/xsdno-xo-so-dak-nong.html">Đắk Nông</a></li><li><a href="/xsgl-xo-so-gia-lai.html">Gia Lai</a></li><li><a href="/xstth-xo-so-hue.html">Huế</a></li><li><a href="/xskh-xo-so-khanh-hoa.html">Khánh Hòa</a></li><li><a href="/xskt-xo-so-kon-tum.html">Kon Tum</a></li><li><a href="/xsnt-xo-so-ninh-thuan.html">Ninh Thuận</a></li><li><a href="/xspy-xo-so-phu-yen.html">Phú Yên</a></li><li><a href="/xsqb-xo-so-quang-binh.html">Quảng Bình</a></li><li><a href="/xsqna-xo-so-quang-nam.html">Quảng Nam</a></li><li><a href="/xsqng-xo-so-quang-ngai.html">Quảng Ngãi</a></li><li><a href="/xsqt-xo-so-quang-tri.html">Quảng Trị</a></li></ul>
+                </li>
+                <li><a href="javascript:void(0)">Miền Nam</a><i class="fa fa-caret-down" aria-hidden="true"></i>
+                    <ul class="level3"><li><a href="/xsag-xo-so-an-giang.html">An Giang</a></li><li><a href="/xsbl-xo-so-bac-lieu.html">Bạc Liêu</a></li><li><a href="/xsbtr-xo-so-ben-tre.html">Bến Tre</a></li><li><a href="/xsbd-xo-so-binh-duong.html">Bình Dương</a></li><li><a href="/xsbp-xo-so-binh-phuoc.html">Bình Phước</a></li><li><a href="/xsbth-xo-so-binh-thuan.html">Bình Thuận</a></li><li><a href="/xscm-xo-so-ca-mau.html">Cà Mau</a></li><li><a href="/xsct-xo-so-can-tho.html">Cần Thơ</a></li><li><a href="/xsdl-xo-so-da-lat.html">Đà Lạt</a></li><li><a href="/xsdn-xo-so-dong-nai.html">Đồng Nai</a></li><li><a href="/xsdt-xo-so-dong-thap.html">Đồng Tháp</a></li><li><a href="/xshg-xo-so-hau-giang.html">Hậu Giang</a></li><li><a href="/xskg-xo-so-kien-giang.html">Kiên Giang</a></li><li><a href="/xsla-xo-so-long-an.html">Long An</a></li><li><a href="/xsst-xo-so-soc-trang.html">Sóc Trăng</a></li><li><a href="/xstn-xo-so-tay-ninh.html">Tây Ninh</a></li><li><a href="/xstg-xo-so-tien-giang.html">Tiền Giang</a></li><li><a href="/xshcm-xo-so-tphcm.html">TP.HCM</a></li><li><a href="/xstv-xo-so-tra-vinh.html">Trà Vinh</a></li><li><a href="/xsvl-xo-so-vinh-long.html">Vĩnh Long</a></li><li><a href="/xsvt-xo-so-vung-tau.html">Vũng Tàu</a></li></ul>
+                </li>
+            </ul></div></div>
+
+</div>
+<div class="col-xs-12 col-sm-12 col-md-3 fix-width-right">
+
+    <h2 class="hide">Right side bar</h2>
+    <div class="hidden-xs hidden-sm item-menu border-margin" style="margin-top:10px">
+        <div class="title-item"><h3><a href="/xo-so-dien-toan-tu-chon-mega-645.html">Xổ Số Điện Toán</a></h3></div>
+        <div class="content-item"><ul>
+                <li><a href="/xo-so-dien-toan-tu-chon-mega-645.html" title="XS Mega 6/45" class="text-black-bold">XS Mega 6/45</a>
+                    <ul class="sub-content">
+                        <li><a href="/xs-mega-thu-4.html" title="XS Mega 6/45 thứ 4">Thứ 4</a></li>
+                        <li><a href="/xs-mega-thu-6.html" title="XS Mega 6/45 thứ 6">Thứ 6</a></li>
+                        <li><a href="/xs-mega-chu-nhat.html" title="XS Mega 6/45 chủ nhật">Chủ Nhật</a></li>
+                    </ul></li>
+                <li><a href="/xo-so-max4d.html" title="XS Max 4D" class="text-black-bold">XS Max 4D</a>
+                    <ul class="sub-content">
+                        <li><a href="/xs-max4d-thu-3.html" title="XS Max 4D thứ 3">Thứ 3</a></li>
+                        <li><a href="/xs-max4d-thu-5.html" title="XS Max 4D thứ 5">Thứ 5</a></li>
+                        <li><a href="/xs-max4d-thu-7.html" title="XS Max 4D thứ 7">Thứ 7</a></li>
+                    </ul></li>
+                <li><a href="/xs-power-xo-so-power-655.html" title="XS Power 6/55" class="text-black-bold">XS Power 6/55</a>
+                    <ul class="sub-content">
+                        <li><a href="/xs-power-655-thu-3.html" title="XS Power thứ 3">Thứ 3</a></li>
+                        <li><a href="/xs-power-655-thu-5.html" title="XS Power thứ 5">Thứ 5</a></li>
+                        <li><a href="/xs-power-655-thu-7.html" title="XS Power thứ 7">Thứ 7</a></li>
+                    </ul></li>
+                <li><a href="/xo-so-max3d.html" title="XS Max 3D" class="text-black-bold">XS Max 3D</a>
+                    <ul class="sub-content">
+                        <li><a href="/xs-max3d-thu-2.html" title="XS Max 3D thứ 2">Thứ 2</a></li>
+                        <li><a href="/xs-max3d-thu-4.html" title="XS Max 3D thứ 4">Thứ 4</a></li>
+                        <li><a href="/xs-max3d-thu-6.html" title="XS Max 3D thứ 6">Thứ 6</a></li>
+                    </ul></li>
+                <li><a href="/xo-so-dien-toan-6x36.html" title="XS Điện toán 6x36" class="text-black-bold">XS Điện toán 6x36</a></li>
+                <li><a href="/xo-so-dien-toan-123.html" title="XS Điện toán 123" class="text-black-bold">XS Điện toán 123</a></li>
+                <li><a href="/xo-so-than-tai.html" title="XS Thần tài 4" class="text-black-bold">XS Thần tài 4</a></li>
+            </ul></div></div>
+    <div class="adv-side-bar">
+
+        <div class="quangcao980x90_nomobile">
+            <script type="text/javascript">if ((window.mobileAndTabletcheck() && 'wap' == 'web') || (window.mobileAndTabletcheck() == false && 'web' == 'web') ) {document.write('<div id="23c97e9cb93576e45d2feaf00d0e8502"></div><scr' + 'ipt async src="https://click.advertnative.com/loading/?handle=2140"></scr' + 'ipt>');}</script><div id="23c97e9cb93576e45d2feaf00d0e8502"></div><script async="" src="https://click.advertnative.com/loading/?handle=2140"></script>
+        </div>
+        <div class="quangcao980x90_nodesktop">
+
+        </div>
+    </div>
+    <div class="item-menu border-margin">
+        <div class="title-item"><h3><a href="/do-so-trung.html">Dò Vé Số</a></h3></div>
+        <div class="form-doveso">
+            <div class="form-inline">
+                <div class="form-group"><label for="ngaydoheader" class="lable-text">Chọn ngày:</label>
+                    <input type="text" id="ngaydoheader" value="16-12-2019" class="form-control form-group" required="required" placeholder="Chọn ngày" onchange="xsdp.getTinhtheoNgay(this.value);" size="9" title="Ngày dò">
+                </div>
+                <div class="form-group"><label for="tinhheader" class="lable-text">Chọn tỉnh:</label>
+                    <select class="form-control form-group" id="tinhheader">
+                        <option value="mb" data-lotterydesc="/xsmb-xo-so-mien-bac.html">Miền Bắc</option>
+                        <option value="TTH" data-lotterydesc="/xstth-xo-so-hue.html">Huế</option>
+                        <option value="PY" data-lotterydesc="/xspy-xo-so-phu-yen.html">Phú Yên</option>
+                        <option value="DT" data-lotterydesc="/xsdt-xo-so-dong-thap.html">Đồng Tháp</option>
+                        <option value="HCM" data-lotterydesc="/xshcm-xo-so-tphcm.html">TP.HCM</option>
+                        <option value="CM" data-lotterydesc="/xscm-xo-so-ca-mau.html">Cà Mau</option>
+                    </select></div>
+                <div class="form-group">
+                    <input type="text" class="form-control form-group" name="nhapso" id="inputNumberDo" required="required" placeholder="Nhập dãy số" size="9" title="Nhập dãy số">
+                    <button type="submit" class="btn btn-red form-group" id="btndoSo" title="Kết quả">Kết quả</button>
+                </div></div>
+            <p class="text-12">( Có thể nhập tối thiểu 2 số, tối đa 17 số )</p>
+        </div></div>
+
+
+
+</div>
